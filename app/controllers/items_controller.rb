0c0613840f90a4182b283c7f2fff5b01ea77a712
manuelsegovia <msegovia@mac.com>
2016-11-25 13:41:29 -0500

    added VOTES capability, added create to Items Controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,19 @@
 class ItemsController < InheritedResources::Base
 before_action :authenticate_user!, except: [:index, :show]
 
-
+def create
+		@item =current_user.items.new(item_params)
+		if @item.save
+			redirect_to @item, notice: 'Item was succesfully created'
+		else
+			render :new
+		end	
+	 end
 protected
 	def begin_of_association_chain
-		current_user 
+		@current_user 
 	end
 	
-
   private
 
     def item_params
---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,14 @@
+class VotesController < ApplicationController
+	before_action :authenticate_user!
+  def create
+  	item = Item.find(params[:id])
+    item.liked_by current_user
+    redirect_to items_path, notice: "You succesfully voted for an item"
+  end
+
+  def destroy
+  	item = Item.find(params[:id])
+    item.unliked_by current_user
+    redirect_to items_path, notice: "You succesfully unvoted for an item"
+  end
+end
